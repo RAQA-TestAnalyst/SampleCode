39316621d9ce63f600fbd0fe01d535637c6c5734
RusalinaAlam-SQA <62039401+RusalinaAlam-SQA@users.noreply.github.com>
2020-08-07 01:01:44 -0400

    Basic

---
+++ src/SampleJavaCodes/Basic.java
@@ -0,0 +1,20 @@
+package SampleJavaCodes;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		
+			int x = 10;
+			
+			int y = 5;
+			
+			String name = "Rusalina Alam";
+			
+			System.out.println("x+y = " + (x+y));
+			System.out.println("x-y = "+(x-y));
+			System.out.println("The name is: " +name);
+			System.out.println(name   + x+y);
+
+	}
+
+}
